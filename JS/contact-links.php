69fe162633530e23e50c0489647b6c440e07312c
Arnel Imperial <9pagemake7@gmail.com>
2019-01-11 08:04:18 +0200

    Modify JS/contact-links.php file

--- JS/contact-links.php
+++ JS/contact-links.php
@@ -1,10 +1,10 @@
 <!-- JS file links -->
-  <!--<script async src="javascript/vendor/modernizr-3.6.0.min.js"></script> -->
-  <!--<script async src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>-->
-  <!--<script>window.jQuery || document.write('<script src="js/vendor/jquery-3.3.1.min.js"><\/script>')</script>-->
+  <script async src="javascript/vendor/modernizr-3.6.0.min.js"></script> -->
+  <script async src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>-->
+  <script>window.jQuery || document.write('<script src="js/vendor/jquery-3.3.1.min.js"><\/script>')</script>-->
   <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
 	
-  <!--<script src="javascript/scroll.js"></script> -->
+  
   <script async src="javascript/plugins.js"></script>
   <script async src="javascript/main-nav.js"></script>
   <script async src="javascript/reset.js"></script>
